a5eb9a3d2542f60ed3aa0ce4029c65c45432ac6a
iampeterkr <i.am.peter.kr@kaist.ac.kr>
2019-07-22 10:13:45 +0900

    Update welcom.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -235,7 +235,7 @@ var store = [{
         "url": "http://localhost:4000/welcome-to-lls/",
         "teaser":null},{
         "title": "[Python Codecademy] 파이썬 코드카데미 한글설명 가이드 프로젝트 블로그 안내!",
-        "excerpt":"[#Python #Codecademy(코드카데미) #한글설명 가이드 #프로젝트 (00-#00)] (#Python#파이썬#Codecademy#코드카데미#한글#정답#solution#무료#free) Welcome 이곳은 [Python Codecademy] 파이썬 코드카데미 한글설명 가이드 프로젝트 블로그 입니다. Python Code cademy(파이썬 코드카데미)의 전체 한글 설명서는 다음 Python 링크에 연결되어 있습니다. Codecademy 를 활용한 무료 python2, python3 교육 관련 자료가 있습니다. POST의 글을 참조하여 해당 자료로 넘어가거나, 상단의 Python 링크를 통하여...","categories": [],
+        "excerpt":"[#Python #Codecademy(코드카데미) #한글설명 가이드 #프로젝트 (00-#00)] (#Python #파이썬 #Codecademy #코드카데미 #한글 # 한국어 #정답 #solution #무료 #free) Welcome 이곳은 [Python Codecademy] 파이썬 코드카데미 한글설명 가이드 프로젝트 블로그 입니다. Codecademy 사에서 자체 한글 서비스는 예전에 지원되었으나, 현재는 지원되지 않고 있습니다. 혼자서 파이썬 공부 하기 좋은 사이트인 Codecademy가 영어로 되어 있다보니, 영어에...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/welcome/",
         "teaser":null},{
